73af15be51b3c09bbec5be3976c2169a5840aa8c
prodige93 <77859813+prodige93@users.noreply.github.com>
2025-05-23 02:52:10 +0200

    Add .next/static/chunks/src_app_dashboard_drafts_page_tsx_58e931be._.js 1747961532111

---
+++ .next/static/chunks/src_app_dashboard_drafts_page_tsx_58e931be._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_dashboard_drafts_page_tsx_70a6fd46._.js"
+],
+    source: "dynamic"
+});
