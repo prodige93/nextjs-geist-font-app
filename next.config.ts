061fc210860e4562d3a446d2c6c85292475e6964
prodige93 <77859813+prodige93@users.noreply.github.com>
2025-05-23 02:52:02 +0200

    Add next.config.ts 1747961524530

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'os.labwearstudios.com',
+        pathname: '/assets/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
