123075c8458f7883e5dec234d5d1e626a26e8373
prodige93 <77859813+prodige93@users.noreply.github.com>
2025-05-23 02:51:48 +0200

    Add .next/static/chunks/src_app_page_tsx_b025fed5._.js 1747961510669

---
+++ .next/static/chunks/src_app_page_tsx_b025fed5._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/app/page.tsx [app-client] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, k: __turbopack_refresh__, m: module, e: exports } = __turbopack_context__;
+{
+const e = new Error(`Could not parse module '[project]/src/app/page.tsx'
+
+'import', and 'export' cannot be used outside of module code`);
+e.code = 'MODULE_UNPARSEABLE';
+throw e;}}),
+}]);
